e676365b53fd92a51d0b889de6a98d69f5214809
Jeff See <jeffsee.55@gmail.com>
2025-10-28 07:18:34 -0700

    init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+export const config ={
+  maxDuration: 90
+}
+
+const welcomeStrings = [
+  "Hello Hono!",
+  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
+]
+
+app.get('/', (c) => {
+  return c.text(welcomeStrings.join('\n\n'))
+})
+
+app.get('/users/:id', (c) => {
+  return c.text(`User ${c.req.param('id')}`)
+})
+
+const admin = new Hono()
+
+admin.get('/', (c) => {
+  return c.text('Admin')
+})
+app.route('/admin', admin)
+
+export default app
